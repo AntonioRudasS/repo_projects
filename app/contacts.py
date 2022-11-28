bc2f914b194a93b2c430fb2cd9db9dc4fe99ffa5
Antonio José Rudas Sanchez <62669845+AntonioRudasS@users.noreply.github.com>
2022-11-27 23:56:04 -0500

    update

--- app/contacts.py
+++ app/contacts.py
@@ -7,63 +7,65 @@ contacts = Blueprint('contacts', __name__, template_folder='app/templates')
 @contacts.route('/')
 def Index():
     cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM contacts')
+    cur.execute('SELECT * FROM usuario')
     data = cur.fetchall()
     cur.close()
     return render_template('index.html', contacts=data)
 
 
-@contacts.route('/add_contact', methods=['POST'])
-def add_contact():
+@contacts.route('/add_project', methods=['POST'])
+def add_project():
     if request.method == 'POST':
-        fullname = request.form['fullname']
-        phone = request.form['phone']
-        email = request.form['email']
+        autor = request.form['autor']
+        titulo = request.form['titulo']
+        objetivo = request.form['objetivo']
+        abstract = request.form['abstract']
         try:
             cur = mysql.connection.cursor()
-            cur.execute(
-                "INSERT INTO contacts (fullname, phone, email) VALUES (%s,%s,%s)", (fullname, phone, email))
+            cur.execute("INSERT INTO usuario (autor, titulo, objetivo, abstract) VALUES (%s,%s,%s)", (autor, titulo, objetivo, abstract))
             mysql.connection.commit()
-            flash('Contact Added successfully')
-            return redirect(url_for('contacts.Index'))
+            flash('Project Added successfully')
+            return redirect(url_for('project.Index'))
         except Exception as e:
             flash(e.args[1])
-            return redirect(url_for('contacts.Index'))
+            return redirect(url_for('project.Index'))
 
 
 @contacts.route('/edit/<id>', methods=['POST', 'GET'])
-def get_contact(id):
+def get_project(id):
     cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM contacts WHERE id = %s', (id))
+    cur.execute('SELECT * FROM usuario WHERE id = %s', (id))
     data = cur.fetchall()
     cur.close()
     print(data[0])
-    return render_template('edit-contact.html', contact=data[0])
+    return render_template('edit-project.html', contact=data[0])
 
 
 @contacts.route('/update/<id>', methods=['POST'])
-def update_contact(id):
+def update_project(id):
     if request.method == 'POST':
-        fullname = request.form['fullname']
-        phone = request.form['phone']
-        email = request.form['email']
+        autor = request.form['autor']
+        titulo = request.form['titulo']
+        objetivo = request.form['objetivo']
+        abstract = request.form['abstract']
         cur = mysql.connection.cursor()
         cur.execute("""
-            UPDATE contacts
-            SET fullname = %s,
-                email = %s,
-                phone = %s
+            UPDATE usuario
+            SET autor = %s,
+                titulo = %s,
+                objetivo = %s,
+                abstract = %s 
             WHERE id = %s
-        """, (fullname, email, phone, id))
-        flash('Contact Updated Successfully')
+        """, (autor, titulo, objetivo, abstract, id))
+        flash('Project Updated Successfully')
         mysql.connection.commit()
-        return redirect(url_for('contacts.Index'))
+        return redirect(url_for('project.Index'))
 
 
 @contacts.route('/delete/<string:id>', methods=['POST', 'GET'])
-def delete_contact(id):
+def delete_project(id):
     cur = mysql.connection.cursor()
-    cur.execute('DELETE FROM contacts WHERE id = {0}'.format(id))
+    cur.execute('DELETE FROM usuario WHERE id = {0}'.format(id))
     mysql.connection.commit()
-    flash('Contact Removed Successfully')
-    return redirect(url_for('contacts.Index'))
+    flash('Project Removed Successfully')
+    return redirect(url_for('project.Index'))
